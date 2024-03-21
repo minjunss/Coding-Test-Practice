96ae9e43decd09a3f06b7fe1816781f6d8a6de7e
minjunss <103651022+minjunss@users.noreply.github.com>
2024-03-21 16:59:11 +0900

    [level 2] Title: 구명보트, Time: 8.29 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,19 @@
+def solution(people, limit):
+    people.sort()
+
+    count = 0
+    left = 0
+    right = len(people) -1
+    
+    while(left <= right):
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        count += 1
+        
+    return count
+
+                
+                
+                
+            
