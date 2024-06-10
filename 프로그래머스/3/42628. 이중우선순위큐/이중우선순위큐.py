bdb76297a9f37266f77bb1ba99367b7f98090407
minjunss <103651022+minjunss@users.noreply.github.com>
2024-06-11 05:59:57 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,19 +1,39 @@
-from collections import deque
+import heapq
+
 def solution(operations):
-    answer = [0,0]
-    queue = deque()
+    answer = [0, 0]
+    min_heap = []
+    max_heap = []
+    entry_count = {}
 
     for operation in operations:
-        a = operation.split(' ')
-        if a[0] == "I":
-            queue.append(int(a[1]))
-        elif queue:
-            if a[1] == "1":
-                queue.remove(max(queue))
+        if operation.startswith("I "):
+            num = int(operation.split()[1])
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            if num in entry_count:
+                entry_count[num] += 1
             else:
-                queue.remove(min(queue))
+                entry_count[num] = 1
+        elif operation == "D 1":
+            while max_heap and entry_count[-max_heap[0]] == 0:
+                heapq.heappop(max_heap)
+            if max_heap:
+                max_value = -heapq.heappop(max_heap)
+                entry_count[max_value] -= 1
+        elif operation == "D -1":
+            while min_heap and entry_count[min_heap[0]] == 0:
+                heapq.heappop(min_heap)
+            if min_heap:
+                min_value = heapq.heappop(min_heap)
+                entry_count[min_value] -= 1
 
-    if queue:
-        answer = [max(queue), min(queue)]
+    while min_heap and entry_count[min_heap[0]] == 0:
+        heapq.heappop(min_heap)
+    while max_heap and entry_count[-max_heap[0]] == 0:
+        heapq.heappop(max_heap)
+
+    if min_heap and max_heap:
+        answer = [-max_heap[0], min_heap[0]]
 
     return answer
